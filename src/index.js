15fa0640a730a6541827193039e654c60c14a1cc
ulquiomaru <baris.gorgulu@gmail.com>
2020-07-28 23:38:12 +0300

    pre-resolvers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,59 @@
 import "dotenv/config";
+import cors from "cors";
+import express from "express";
+import { ApolloServer, gql } from "apollo-server-express";
 
-console.log("Hello Project");
+const app = express();
 
-console.log(process.env.MY_SECRET);
+app.use(cors());
+
+const schema = gql`
+  type Query {
+    users: [User!]
+    user(id: ID!): User
+    me: User
+  }
+
+  type User {
+    id: ID!
+    username: String!
+  }
+`;
+
+let users = {
+  1: {
+    id: "1",
+    username: "Robin Wieruch",
+  },
+  2: {
+    id: "2",
+    username: "Dave Davids",
+  },
+};
+
+const me = users[1];
+
+const resolvers = {
+  Query: {
+    users: () => {
+      return Object.values(users);
+    },
+    user: (parent, { id }) => {
+      return users[id];
+    },
+    me: () => {
+      return me;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
+
+server.applyMiddleware({ app, path: "/graphql" });
+
+app.listen({ port: 8000 }, () => {
+  console.log("Apollo Server on http://localhost:8000/graphql");
+});
