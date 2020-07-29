198254462a7be634da3ba86c3f0f3f91b94f4dfa
Barış Görgülü <baris.gorgulu@gmail.com>
2020-07-29 15:46:17 +0300

    cursor pagination

--- src/resolvers/message.js
+++ src/resolvers/message.js
@@ -3,6 +3,12 @@ import { combineResolvers } from 'graphql-resolvers';
 
 import { isAuthenticated, isMessageOwner } from './authorization';
 
+const toCursorHash = (string) =>
+  Buffer.from(string).toString('base64');
+
+const fromCursorHash = (string) =>
+  Buffer.from(string, 'base64').toString('ascii');
+
 export default {
   Query: {
     messages: async (parent, { cursor, limit = 100 }, { models }) => {
@@ -10,17 +16,30 @@ export default {
         ? {
             where: {
               createdAt: {
-                [Sequelize.Op.lt]: cursor,
+                [Sequelize.Op.lt]: fromCursorHash(cursor),
               },
             },
           }
         : {};
 
-      return await models.Message.findAll({
+      const messages = await models.Message.findAll({
         order: [['createdAt', 'DESC']],
-        limit,
+        limit: limit + 1,
         ...cursorOptions,
       });
+
+      const hasNextPage = messages.length > limit;
+      const edges = hasNextPage ? messages.slice(0, -1) : messages;
+
+      return {
+        edges,
+        pageInfo: {
+          hasNextPage,
+          endCursor: toCursorHash(
+            edges[edges.length - 1].createdAt.toString(),
+          ),
+        },
+      };
     },
     message: async (parent, { id }, { models }) => {
       return await models.Message.findByPk(id);
